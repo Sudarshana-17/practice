01ebd8316698d06770271141a153b9619cd96b3f
Sudarshana-17 <sudarshanw7353@gmail.com>
2023-12-27 12:37:24 +0530

    l

---
+++ full stack/front end/languages/ts/datatpes/own-datatypes/owndata.ts
@@ -0,0 +1,11 @@
+class waterbottle{
+    plastic:boolean
+    cap:boolean
+    shape:string
+    withcap(){
+        console.log("we can close bottle")
+    }
+}
+
+var bislary=new waterbottle()
+bislary.withcap()
