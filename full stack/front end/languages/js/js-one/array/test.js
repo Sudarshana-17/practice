1f43afc42d094bf28e889066b680c969961ca7f2
Sudarshana-17 <sudarshanw7353@gmail.com>
2024-01-02 10:05:05 +0530

    l

---
+++ full stack/front end/languages/js/js-one/array/test.js
@@ -0,0 +1,10 @@
+var human = /** @class */ (function () {
+    function human() {
+        this.eyes = 2;
+    }
+    human.prototype.witheyes = function () {
+    };
+    return human;
+}());
+var sudarshan = new human();
+console.log(sudarshan);
